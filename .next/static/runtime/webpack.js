2ce004d27f2beb86b18c0e2b4292a7b3b371fd46
Sylvain <sylvoz75@gmail.com>
2018-11-27 21:14:20 +1100

    deleteITem

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "dd907432f596d25214ce";
+/******/ 	var hotCurrentHash = "1d18a5a50e24660fb951";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
